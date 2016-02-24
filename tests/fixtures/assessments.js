cdb0407cde8234c2df59bbeeda47e8b4a07e8a6c
Camille Reynders <camille.reynders@gmail.com>
2016-02-24 14:56:22 +0100

    (Assessments) update comparisonsNum

--- tests/fixtures/assessments.js
+++ tests/fixtures/assessments.js
@@ -1,6 +1,5 @@
 'use strict';
-var _ = require('lodash');
-
+var _ = require( 'lodash' );
 
 module.exports.items = {
   valid: {},
@@ -10,11 +9,11 @@ module.exports.items = {
 module.exports.items.valid.minimal = {
   "_id": "56121dc75f699bd58181ab13",
   "stage": 0,
-  "comparisonsNum": {
-    "perRepresentation": 0,
-    "perAssessor": [
-      200
-    ]
+  "limits": {
+    "comparisonsNum": {
+      "perRepresentation": 0,
+      "perAssessor": 200
+    }
   }
 };
 
@@ -30,11 +29,11 @@ module.exports.items.valid.full = {
   "uiCopy": "{\r\n    \"phase_selection\": {\r\n        \"title\": \"Beoordeel\",\r\n        \"description\": \"Geef aan welke tekst beter geschreven is in functie van  de competentie 'argumentatief schrijven'\",\r\n        \"a_button\": {\r\n            \"label\" : \"Tekst A is beter\",\r\n            \"tooltip\": \"Klik om aan te duiden dat tekst A beter is dan B.\"\r\n        },\r\n        \"b_button\": {\r\n            \"label\": \"Tekst B is beter\",\r\n            \"tooltip\": \"Klik om aan te duiden dat tekst B beter is dan A.\"\r\n        }\r\n    },\r\n    \"phase_comparative\": {\r\n        \"title\": \"Vergelijk\",\r\n        \"description\": \"Licht kort je keuze toe\"\r\n    },\r\n    \"phase_pros-cons\": {\r\n        \"title\": \"Vergelijk\",\r\n        \"description\": \"Beschrijf hier wat je positief en negatief vond aan de teksten:\",\r\n        \"a_title\": \"Tekst A\",\r\n        \"b_title\": \"Tekst B\",\r\n        \"positive\": \"Positief\",\r\n        \"negative\": \"Negatief\"\r\n    },\r\n    \"phase_passfail\":{\r\n        \"title\":\"Geslaagd of niet?\",\r\n        \"description\" : \"Duid aan of deze teksten volgens jou geslaagd zijn voor de opdracht.\",\r\n        \"options\": {\r\n          \"passed\" : {\r\n              \"label\" : \"Geslaagd\",\r\n              \"icon\" : \"ok\"\r\n          },\r\n          \"failed\" : { \r\n              \"label\":\"Niet geslaagd\", \r\n              \"icon\":\"remove\"\r\n          },\r\n          \"undecided\" : { \r\n              \"label\" : \"Weet het niet\", \r\n              \"icon\": \"question-sign\"\r\n          }\r\n        }\r\n    },\r\n    \"phase_seq-selection\": {\r\n\t    \"title\": \"Hoe moeilijk vond je het om de keuze te maken?\"\r\n\t},\r\n  \t\"phase_seq-comparative\": {\r\n    \t\"title\": \"Hoe moeilijk vond je het om je keuze te beargumenteren?\"\r\n\t},\r\n    \"phase_seq-passfail\": {\r\n    \t\"title\": \"Hoe moeilijk vond je het om geslaagd/niet geslaagd te kiezen?\"\r\n\t},\r\n    \"representation_viewer\": {\r\n        \"pdf\":{\r\n            \"tooltip\": \"Gebruik de knoppen rechts bovenaan om de paper fullscreen te bekijken of te downloaden.\"\r\n        },\r\n        \"image\": {\r\n            \"tooltip\": \"Klik om deze tekst te selecteren\"\r\n        }\r\n    },\r\n    \"notes\" : {\r\n        \"label\" : \"Notities\",\r\n        \"tooltip\" : \"Noteer hier je opmerkingen bij deze tekst.\"\r\n    }\r\n}\r\n",
   "enableTimeLogging": false,
   "stage": 0,
-  "comparisonsNum": {
-    "perRepresentation": 0,
-    "perAssessor": [
-      200
-    ]
+  "limits": {
+    "comparisonsNum": {
+      "perRepresentation": 0,
+      "perAssessor": 200
+    }
   },
   "phases": [
     "556444845cbc476e7934c2ba",
